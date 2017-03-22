6dd7902e96db67acf137b8f286d4121d4f506d50
Jorge Alberto Díaz Orozco <jorge.orozco@datys.cu>
2017-03-22 09:31:07 -0400

    adding proxy support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rocketchat_API',
-    version='0.3',
+    version='0.3.1',
     packages=['rocketchat_API', 'rocketchat_API.APIExceptions'],
     url='https://github.com/jadolg/rocketchat_API',
     license='MIT',
