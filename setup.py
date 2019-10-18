8b08b5b3936260f2dde0c1868d8070a1d355a660
akiel <akiel@aleph.engineering>
2019-10-18 16:51:52 -0400

    Update to version 0.6.35

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='rocketchat_API',
-    version='0.6.34',
+    version='0.6.35',
     packages=['rocketchat_API', 'rocketchat_API.APIExceptions'],
     url='https://github.com/jadolg/rocketchat_API',
     license='MIT',
