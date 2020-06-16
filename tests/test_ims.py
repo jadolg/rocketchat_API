217f9657dd267e4880ba92935bb09c30d29d05ae
Marshall "Gripp" Gillson <marshall@glassEyeballs.com>
2020-06-16 17:12:18 -0500

    Added test.

--- tests/test_ims.py
+++ tests/test_ims.py
@@ -68,6 +68,10 @@ def test_im_history(logged_rocket, recipient_user):
 def test_im_messages(logged_rocket, recipient_user):
     im_message = logged_rocket.im_messages(username=recipient_user).json()
     assert im_message.get('success')
+    im_create = logged_rocket.im_create(recipient_user).json()
+    room_id = im_create.get('room').get('_id')
+    im_message = logged_rocket.im_messages_others(room_id=room_id).json()
+    assert im_message.get('success')
 
 
 def test_im_messages_others(logged_rocket, recipient_user):
