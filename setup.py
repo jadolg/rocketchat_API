e91623c44216d177e485cb586c2a7a738fc62a0c
akiel <diazorozcoj@gmail.com>
2020-09-12 12:15:12 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='rocketchat_API',
-    version='1.8.0',
+    version='1.9.0',
     packages=['rocketchat_API', 'rocketchat_API.APIExceptions'],
     url='https://github.com/jadolg/rocketchat_API',
     license='MIT',
