7cc3c121f35ded41f5116ce6c74b823336340f20
Krzysztof Stępniak <kfs.szk@gmail.com>
2020-11-06 20:01:24 +0100

    added test for update_jitsi_timeout

--- rocketchat_API/rocketchat.py
+++ rocketchat_API/rocketchat.py
@@ -1031,5 +1031,5 @@ class RocketChat:
     def update_jitsi_timeout(self, room_id, **kwargs):
         """Updates the timeout of Jitsi video conference in a channel."""
         return self.__call_api_post(
-            'video-conference/jitsi.update-timeout', roomId=room_id, kwargs=kwargs
+            "video-conference/jitsi.update-timeout", roomId=room_id, kwargs=kwargs
         )
