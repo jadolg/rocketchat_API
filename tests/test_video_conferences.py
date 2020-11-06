14804bb8b0fb1203cb93ba16654d9092bc170bbe
Krzysztof Stępniak <kfs.szk@gmail.com>
2020-11-06 20:15:10 +0100

    fixed one small thing

--- tests/test_video_conferences.py
+++ tests/test_video_conferences.py
@@ -1,3 +1,3 @@
 def test_update_jitsi_timeout(logged_rocket):
     update_jitsi_timeout = logged_rocket.update_jitsi_timeout(room_id="GENERAL").json()
-    assert update_jitsi_timeout.get("success") == False
+    assert update_jitsi_timeout.get("success")
