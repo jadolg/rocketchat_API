3c6322853a06fb67d3cc6e91efe8f15024de574f
akiel <diazorozcoj@gmail.com>
2020-11-15 19:07:46 +0100

    Update version to try the new publish workflow

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="rocketchat_API",
-    version="1.10.0",
+    version="1.10.1",
     packages=["rocketchat_API", "rocketchat_API.APIExceptions"],
     url="https://github.com/jadolg/rocketchat_API",
     license="MIT",
