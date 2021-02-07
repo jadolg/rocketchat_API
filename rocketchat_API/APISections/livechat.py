00859457132084b116af06f4645bcfe021838e0d
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-02-07 02:06:23 +0100

    Add API method for livechat inquiries.list

--- rocketchat_API/APISections/livechat.py
+++ rocketchat_API/APISections/livechat.py
@@ -5,3 +5,7 @@ class RocketChatLivechat(RocketChatBase):
     def livechat_rooms(self, **kwargs):
         """Retrieves a list of livechat rooms."""
         return self.call_api_get("livechat/rooms", kwargs=kwargs)
+
+    def livechat_inquiries_list(self, **kwargs):
+        """Lists all of the open livechat inquiries."""
+        return self.call_api_get("livechat/inquiries.list", kwargs=kwargs)
--- tests/test_livechat.py
+++ tests/test_livechat.py
@@ -2,3 +2,9 @@ def test_livechat_rooms(logged_rocket):
     livechat_rooms = logged_rocket.livechat_rooms().json()
     assert livechat_rooms.get("success")
     assert "rooms" in livechat_rooms
+
+
+def test_livechat_inquiries_list(logged_rocket):
+    livechat_inquiries_list = logged_rocket.livechat_inquiries_list().json()
+    assert livechat_inquiries_list.get("success")
+    assert "inquiries" in livechat_inquiries_list
