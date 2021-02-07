8c22e24976d0acf789e4c80e7a3b633269532845
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-02-07 02:15:22 +0100

    Add API method for livechat inquiries.take

--- rocketchat_API/APISections/livechat.py
+++ rocketchat_API/APISections/livechat.py
@@ -9,3 +9,9 @@ class RocketChatLivechat(RocketChatBase):
     def livechat_inquiries_list(self, **kwargs):
         """Lists all of the open livechat inquiries."""
         return self.call_api_get("livechat/inquiries.list", kwargs=kwargs)
+
+    def livechat_inquiries_take(self, inquiry_id, **kwargs):
+        """Takes an open inquiry."""
+        return self.call_api_post(
+            "livechat/inquiries.take", inquiryId=inquiry_id, kwargs=kwargs
+        )
--- tests/test_livechat.py
+++ tests/test_livechat.py
@@ -8,3 +8,12 @@ def test_livechat_inquiries_list(logged_rocket):
     livechat_inquiries_list = logged_rocket.livechat_inquiries_list().json()
     assert livechat_inquiries_list.get("success")
     assert "inquiries" in livechat_inquiries_list
+
+
+def test_livechat_inquiries_take(logged_rocket):
+    # TODO: Find a way of creating an inquiry to be able to properly test this method
+    livechat_inquiries_take = logged_rocket.livechat_inquiries_take(
+        inquiry_id="NotARealThing"
+    ).json()
+    assert not livechat_inquiries_take.get("success")
+    assert "Inquiry already taken" == livechat_inquiries_take.get("reason")
