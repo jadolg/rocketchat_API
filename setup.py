36c531b0896ad45e31269465bf2f9643cb4f8b75
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-10-09 21:01:31 +0200

    bump version to 1.20.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="rocketchat_API",
-    version="1.19.0",
+    version="1.20.0",
     packages=[
         "rocketchat_API",
         "rocketchat_API.APIExceptions",
