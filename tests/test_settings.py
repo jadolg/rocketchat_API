c99c308c59df029502c11ed75df76773e1b5eff4
Kai <sistason@sistason.de>
2022-05-06 23:45:45 +0200

    added tests for the direct, workaround api_calls in test_settings_oauth

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -21,18 +21,20 @@ def test_settings_public(rocket):
 
 def test_settings_oauth(logged_rocket):
     # refresh is not done with any API call ever, so we need to call it manually here
-    logged_rocket.call_api_post(
+    response = logged_rocket.call_api_post(
         "method.call/refreshOAuthService",
         message='{"method": "refreshOAuthService", "params": []}',
     )
+    assert response.ok
     oauth_get = logged_rocket.settings_oauth().json()
     assert oauth_get.get("success")
     if oauth_get.get("services"):
         # remove the OAuth app Test beforehand, when this is not the first test run (for reproducibility)
-        logged_rocket.call_api_post(
+        response = logged_rocket.call_api_post(
             "method.call/removeOAuthService",
             message='{"method": "removeOAuthService", "params": ["Test"]}',
         )
+        assert response.ok
         oauth_get = logged_rocket.settings_oauth().json()
         assert not oauth_get.get("services")
 
