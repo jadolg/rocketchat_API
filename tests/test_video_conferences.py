514be833dd759c51a2b3f8ca0ae2df4499d63b83
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2022-08-09 19:47:55 +0200

    Add todo in the test_update_jitsi_timeout so it's easier to find in the future

--- tests/test_video_conferences.py
+++ tests/test_video_conferences.py
@@ -1,6 +1,7 @@
 import pytest
 
 
+# TODO: Go back to this test once the ticket has being answered
 @pytest.mark.skip(
     reason="Broken in 5.0. https://github.com/RocketChat/Rocket.Chat/issues/26520"
 )
