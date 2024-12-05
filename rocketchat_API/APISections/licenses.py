3160abcd5046eb595dce1df6717b3391cf1b6450
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2024-12-05 13:59:27 +0100

    Remove deprecation warning
    the decorator is only available in the latest python version

--- rocketchat_API/APISections/licenses.py
+++ rocketchat_API/APISections/licenses.py
@@ -1,10 +1,7 @@
-from warnings import deprecated
-
 from rocketchat_API.APISections.base import RocketChatBase
 
 
 class RocketChatLicenses(RocketChatBase):
-    @deprecated("Use licenses_info instead")
     def licenses_get(self, **kwargs):
         """Retrieves a list of all registered licenses in the workspace."""
         return self.call_api_get("licenses.get", kwargs=kwargs)
