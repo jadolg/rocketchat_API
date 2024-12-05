a53e6ec8f32e23354dfe9b1d7c472a0aeff87f6b
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2024-12-05 13:50:01 +0100

    Add new license method and skip the deprecated one when testing v7

--- rocketchat_API/APISections/licenses.py
+++ rocketchat_API/APISections/licenses.py
@@ -1,7 +1,16 @@
+from warnings import deprecated
+
 from rocketchat_API.APISections.base import RocketChatBase
 
 
 class RocketChatLicenses(RocketChatBase):
+    @deprecated("Use licenses_info instead")
     def licenses_get(self, **kwargs):
         """Retrieves a list of all registered licenses in the workspace."""
         return self.call_api_get("licenses.get", kwargs=kwargs)
+
+    def licenses_info(self, **kwargs):
+        """Retrieves a list of all registered licenses in the workspace."""
+        return self.call_api_get("licenses.info", kwargs=kwargs)
+
+
--- tests/test_licenses.py
+++ tests/test_licenses.py
@@ -1,3 +1,10 @@
-def test_licenses_get(logged_rocket):
+from unittest import skipIf
+
+
+def test_licenses_get(logged_rocket, skip_v7):
     licenses_get = logged_rocket.licenses_get().json()
     assert licenses_get.get("success")
+
+def test_licenses_info(logged_rocket):
+    licenses_info = logged_rocket.licenses_info().json()
+    assert licenses_info.get("success")
