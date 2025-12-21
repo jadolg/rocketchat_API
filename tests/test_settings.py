5bda731c75bec2606428dc6af21c55cf11e603fd
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2025-12-21 21:24:31 +0100

    Remove methods that can't be tested

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -16,34 +16,6 @@ def test_settings_public(rocket):
     assert "settings" in settings_public
 
 
-@pytest.mark.skip(
-    reason="Broken in 5.0 https://github.com/jadolg/rocketchat_API/issues/168"
-)
-def test_settings_oauth(logged_rocket):
-    # refresh is not done with any API call ever, so we need to call it manually here
-    response = logged_rocket.call_api_post(
-        "method.call/refreshOAuthService",
-        message='{"method": "refreshOAuthService", "params": []}',
-    )
-    assert response.ok
-    oauth_get = logged_rocket.settings_oauth()
-    if oauth_get.get("services"):
-        # remove the OAuth app Test beforehand, when this is not the first test run (for reproducibility)
-        response = logged_rocket.call_api_post(
-            "method.call/removeOAuthService",
-            message='{"method": "removeOAuthService", "params": ["Test"]}',
-        )
-        assert response.ok
-        oauth_get = logged_rocket.settings_oauth()
-        assert not oauth_get.get("services")
-
-    logged_rocket.settings_addcustomoauth("Test")
-    logged_rocket.settings_update("Accounts_OAuth_Custom-Test", True)
-    time.sleep(3)
-    oauth_get = logged_rocket.settings_oauth()
-    assert oauth_get.get("services")[0].get("service") == "test"
-
-
 def test_service_configurations(rocket):
     service_configurations = rocket.service_configurations()
     assert "configurations" in service_configurations
