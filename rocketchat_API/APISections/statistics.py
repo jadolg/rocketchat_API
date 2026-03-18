4011a884fe198414a281ae3f5d2d5f38d5a3a8d1
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2026-03-18 10:10:07 +0000

    Add automatic pagination to statistics list retrieval (#366)

--- rocketchat_API/APISections/statistics.py
+++ rocketchat_API/APISections/statistics.py
@@ -1,4 +1,4 @@
-from rocketchat_API.APISections.base import RocketChatBase
+from rocketchat_API.APISections.base import RocketChatBase, paginated
 
 
 class RocketChatStatistics(RocketChatBase):
@@ -6,6 +6,7 @@ class RocketChatStatistics(RocketChatBase):
         """Retrieves the current statistics"""
         return self.call_api_get("statistics", kwargs=kwargs)
 
+    @paginated("statistics")
     def statistics_list(self, **kwargs):
         """Selectable statistics about the Rocket.Chat server."""
         return self.call_api_get("statistics.list", kwargs=kwargs)
